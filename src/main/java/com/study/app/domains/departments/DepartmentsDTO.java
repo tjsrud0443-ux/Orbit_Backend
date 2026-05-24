bcbdb91514f405096ecf91fd2317011010a86b8c
정선경 <wjdtjsrud000@naver.com>
2026-05-24 14:27:38 +0900

    조직도 백엔드 구성 진행 중

--- src/main/java/com/study/app/domains/departments/DepartmentsDTO.java
+++ src/main/java/com/study/app/domains/departments/DepartmentsDTO.java
@@ -1,4 +1,61 @@
 package com.study.app.domains.departments;
 
 public class DepartmentsDTO {
+	
+	private Long dept_seq;
+	private String dept_name;
+	private String dept_code;
+	private Long parent_dept_seq;
+	private String created_at;
+	
+	public DepartmentsDTO() {}
+
+	public DepartmentsDTO(Long dept_seq, String dept_name, String dept_code, Long parent_dept_seq, String created_at) {
+		super();
+		this.dept_seq = dept_seq;
+		this.dept_name = dept_name;
+		this.dept_code = dept_code;
+		this.parent_dept_seq = parent_dept_seq;
+		this.created_at = created_at;
+	}
+
+	public Long getDept_seq() {
+		return dept_seq;
+	}
+
+	public void setDept_seq(Long dept_seq) {
+		this.dept_seq = dept_seq;
+	}
+
+	public String getDept_name() {
+		return dept_name;
+	}
+
+	public void setDept_name(String dept_name) {
+		this.dept_name = dept_name;
+	}
+
+	public String getDept_code() {
+		return dept_code;
+	}
+
+	public void setDept_code(String dept_code) {
+		this.dept_code = dept_code;
+	}
+
+	public Long getParent_dept_seq() {
+		return parent_dept_seq;
+	}
+
+	public void setParent_dept_seq(Long parent_dept_seq) {
+		this.parent_dept_seq = parent_dept_seq;
+	}
+
+	public String getCreated_at() {
+		return created_at;
+	}
+
+	public void setCreated_at(String created_at) {
+		this.created_at = created_at;
+	}
 }
--- src/main/java/com/study/app/domains/users/RankDTO.java
+++ src/main/java/com/study/app/domains/users/RankDTO.java
@@ -1,4 +1,36 @@
 package com.study.app.domains.users;
 
 public class RankDTO {
+	
+	private Long rank_seq;
+	private String rank_name;
+	private Long rank_order;
+	
+	public RankDTO() {}
+	
+	public RankDTO(Long rank_seq, String rank_name, Long rank_order) {
+		super();
+		this.rank_seq = rank_seq;
+		this.rank_name = rank_name;
+		this.rank_order = rank_order;
+	}
+	
+	public Long getRank_seq() {
+		return rank_seq;
+	}
+	public void setRank_seq(Long rank_seq) {
+		this.rank_seq = rank_seq;
+	}
+	public String getRank_name() {
+		return rank_name;
+	}
+	public void setRank_name(String rank_name) {
+		this.rank_name = rank_name;
+	}
+	public Long getRank_order() {
+		return rank_order;
+	}
+	public void setRank_order(Long rank_order) {
+		this.rank_order = rank_order;
+	}
 }
