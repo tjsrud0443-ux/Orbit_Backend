e6b6f2d4c09816e9b8c0ed7413f7df3e2261630e
jimin <a@b.com>
2026-06-11 20:52:11 +0900

    비품 신청 관리

--- src/main/java/com/study/app/domains/supplies/SupplyDAO.java
+++ src/main/java/com/study/app/domains/supplies/SupplyDAO.java
@@ -1,6 +1,7 @@
 package com.study.app.domains.supplies;
 
 import java.util.List;
+import java.util.Map;
 
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +15,24 @@ public class SupplyDAO {
 	/*비품 관리*/
 	public List<SupplyDTO> getSupplyList(){
 		return mybatis.selectList("Supply.getSupplyList");
+	}
+	
+	public List<SupplyRequestDTO> getAdminRequestList(Map<String, Object> params){
+		return mybatis.selectList("Supply.getAdminRequestList",params);
+	}
+	
+	public int getAdminRequestCount(Map<String, Object> params) {
+		return mybatis.selectOne("Supply.getAdminRequestCount",params);
+	}
+	
+	//비품 신청 상태 수정
+	public int updateRequestStatus(SupplyRequestDTO dto) {
+		return mybatis.update("Supply.updateRequestStatus", dto);
+	}
+	
+	//비품 재고 수정
+	public int decreaseStock(SupplyRequestItemsDTO dto) {
+		return mybatis.update("Supply.decreaseStock",dto);
 	}
 	
 	/*비품 신청 & 신청된 비품 리스트*/
--- src/main/java/com/study/app/domains/supplies/SupplyRequestDTO.java
+++ src/main/java/com/study/app/domains/supplies/SupplyRequestDTO.java
@@ -6,57 +6,82 @@ import java.util.List;
 public class SupplyRequestDTO {
 	private Long req_seq;
     private String users_id;
-    private LocalDate req_date;
+    private LocalDate req_date;//신청일-> 수령 희망 날짜로 변경
     private String reason;
     private String status;
+    //db에 없다
+    private String user_name;
     private List<SupplyRequestItemsDTO> items;
     
     public SupplyRequestDTO() {}
+
 	public SupplyRequestDTO(Long req_seq, String users_id, LocalDate req_date, String reason, String status,
-			List<SupplyRequestItemsDTO> items) {
+			String user_name, List<SupplyRequestItemsDTO> items) {
 		super();
 		this.req_seq = req_seq;
 		this.users_id = users_id;
 		this.req_date = req_date;
 		this.reason = reason;
 		this.status = status;
+		this.user_name = user_name;
 		this.items = items;
 	}
+
+
 	public Long getReq_seq() {
 		return req_seq;
 	}
+
 	public void setReq_seq(Long req_seq) {
 		this.req_seq = req_seq;
 	}
+
 	public String getUsers_id() {
 		return users_id;
 	}
+
 	public void setUsers_id(String users_id) {
 		this.users_id = users_id;
 	}
+
 	public LocalDate getReq_date() {
 		return req_date;
 	}
+
 	public void setReq_date(LocalDate req_date) {
 		this.req_date = req_date;
 	}
+
 	public String getReason() {
 		return reason;
 	}
+
 	public void setReason(String reason) {
 		this.reason = reason;
 	}
+
 	public String getStatus() {
 		return status;
 	}
+
 	public void setStatus(String status) {
 		this.status = status;
 	}
+
+	public String getUser_name() {
+		return user_name;
+	}
+
+	public void setUser_name(String user_name) {
+		this.user_name = user_name;
+	}
+
 	public List<SupplyRequestItemsDTO> getItems() {
 		return items;
 	}
+
 	public void setItems(List<SupplyRequestItemsDTO> items) {
 		this.items = items;
 	}
-    
+	
 }
